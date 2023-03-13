9324252742dc03fc900f696dce05379cfec87d24
Ali Ahmed <ali.ali.ameen245@gmail.com>
2023-03-13 17:33:14 +0200

    Added unit testing for batteries and engines

---
+++ test/test_battery.py
@@ -0,0 +1,32 @@
+import unittest
+from battery.models.spindler_battery import SpindlerBattery
+from battery.models.nubbin_battery import NubbinBattery
+from datetime import datetime
+
+
+class TestSpindler(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        current_date = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 3)
+        battery = SpindlerBattery(last_service_date, current_date)
+        self.assertTrue(battery.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+        current_date = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 1)
+        battery = SpindlerBattery(last_service_date, current_date)
+        self.assertFalse(battery.needs_service())
+
+
+class TestNubbin(unittest.TestCase):
+    def test_battery_should_be_serviced(self):
+        current_date = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 4)
+        battery = NubbinBattery(last_service_date, current_date)
+        self.assertTrue(battery.needs_service())
+
+    def test_battery_should_not_be_serviced(self):
+        current_date = datetime.today().date()
+        last_service_date = current_date.replace(year=current_date.year - 3)
+        battery = NubbinBattery(last_service_date, current_date)
+        self.assertFalse(battery.needs_service())
